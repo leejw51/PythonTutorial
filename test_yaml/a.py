c7cb5016572e7164b2e70e07dbc78bbd5533e18e
jongwhan <leejw51@gmail.com>
2021-09-10 16:43:38 +0900

    format change

---
+++ test_yaml/a.py
@@ -0,0 +1,30 @@
+import yaml
+import json
+import tomlkit
+
+
+with open("a.yaml") as f: 
+    b= yaml.load(f, Loader=yaml.FullLoader)
+
+myjson=json.dumps(b, indent=4)
+print(myjson)
+
+with open("a.json","w") as f: 
+    f.write(myjson)
+
+
+with open("a.json") as f:
+    myjson2= json.load(f)
+
+myyaml= yaml.dump(myjson2)
+print(myyaml)
+
+with open("b.yaml", "w") as f:
+    f.write(myyaml)
+    
+    
+mytoml=tomlkit.dumps(myjson2)
+print(mytoml)
+
+with open("c.toml", "w") as f:
+    f.write(mytoml)
