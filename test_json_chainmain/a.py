c064bc33d5ddb5a0042883bda4a46c46cadf79cc
jongwhan <leejw51@gmail.com>
2021-09-13 22:16:52 +0900

    ibc only

--- test_json_chainmain/a.py
+++ test_json_chainmain/a.py
@@ -5,8 +5,12 @@ import toml
 
 with open ("a.json") as f :
     a=f.read()
-    b= json.loads(a)
-    c=json.dumps(b, indent=4)
+    b1= json.loads(a)    
+    cap=b1["app_state"]["capability"]
+    ibc=b1["app_state"]["ibc"]
+    del ibc["client_genesis"]
+    b={"capability": cap, "ibc": ibc}    
+    c=json.dumps(b, indent=4)    
     print(c)
     
 with open ("b.json", "w") as f:
--- test_json_cronos/a.py
+++ test_json_cronos/a.py
@@ -5,7 +5,11 @@ import toml
 
 with open ("a.json") as f :
     a=f.read()
-    b= json.loads(a)
+    b1= json.loads(a)    
+    cap=b1["app_state"]["capability"]
+    ibc=b1["app_state"]["ibc"]
+    del ibc["client_genesis"]
+    b={"capability": cap, "ibc": ibc}    
     c=json.dumps(b, indent=4)
     print(c)
     
@@ -20,3 +24,6 @@ with open ("b.toml", "w") as f:
     c=toml.dumps(b)    
     f.write(c)
     
+    
+    
+          
